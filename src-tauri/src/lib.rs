c873a5c3f09cffe5636cf1127314c66fb60006ad
trojanking123 <wuqianking123@gmail.com>
2024-09-14 16:15:46 +0800

    feat: deep link test

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,4 +1,15 @@
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
+
+use tauri::Emitter;
+use tauri::Listener;
+use tauri::Manager;
+use tauri_plugin_deep_link::DeepLinkExt;
+
+#[derive(Clone, serde::Serialize)]
+struct Payload {
+    args: Vec<String>,
+    cwd: String,
+}
 #[tauri::command]
 fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
@@ -6,8 +17,34 @@ fn greet(name: &str) -> String {
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
-    tauri::Builder::default()
-        .plugin(tauri_plugin_shell::init())
+    let mut app_builder = tauri::Builder::default();
+    #[cfg(desktop)]
+    {
+        app_builder = app_builder.plugin(tauri_plugin_single_instance::init(|app, argv, cwd| {
+            println!("{}, {argv:?}, {cwd}", app.package_info().name);
+            app.emit("single-instance", Payload { args: argv, cwd }).unwrap();
+        }));
+    }
+    
+    app_builder.plugin(tauri_plugin_shell::init())
+        .plugin(tauri_plugin_deep_link::init())
+        .setup(|app| {
+            // ensure deep links are registered on the system
+            // this is useful because AppImages requires additional setup to be available in the system
+            // and calling register() makes the deep links immediately available - without any user input
+            #[cfg(target_os = "linux")]
+            {
+                use tauri_plugin_deep_link::DeepLinkExt;
+                app.deep_link().register_all()?;
+            }
+
+            app.deep_link().register("watery")?;
+            app.listen("single-instance", |url| {
+                dbg!("--------");
+                dbg!(url);
+            });
+            Ok(())
+        })
         .invoke_handler(tauri::generate_handler![greet])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,6 +1,12 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use cfg_aliases::cfg_aliases;
+
 fn main() {
+    cfg_aliases! {
+        desktop: {any(targetos = "linux", targetos = "macos", targetos="windows")},
+    }
+
     watery_lib::run()
 }
