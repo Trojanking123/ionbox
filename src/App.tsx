bc1638d67c40655eb48605baffeb8af949787a07
trojanking123 <wuqianking123@gmail.com>
2024-09-19 16:03:38 +0800

    chore: format code

--- src/App.tsx
+++ src/App.tsx
@@ -1,99 +1,89 @@
-import { useState } from "react";
+import { Button } from "@/components/ui/button";
 import { invoke } from "@tauri-apps/api/core";
-import { Button } from "@/components/ui/button"
-import { onOpenUrl } from '@tauri-apps/plugin-deep-link';
+import { onOpenUrl } from "@tauri-apps/plugin-deep-link";
+import { useState } from "react";
 
 import "./styles.css";
+import OpenLink from "@/components/openlink";
 import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card"
-import OpenLink from "@/components/openlink"
+	Card,
+	CardContent,
+	CardDescription,
+	CardFooter,
+	CardHeader,
+	CardTitle,
+} from "@/components/ui/card";
 
-import {
-  Avatar,
-  AvatarFallback,
-  AvatarImage,
-} from "@/components/ui/avatar"
-
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 
 await onOpenUrl((urls) => {
-  console.log('deep link:', urls);
+	console.log("deep link:", urls);
 });
 
 function App() {
-  const [greetMsg, setGreetMsg] = useState("");
-  const [name, setName] = useState("");
+	const [greetMsg, setGreetMsg] = useState("");
+	const [name, setName] = useState("");
 
-  async function greet() {
-    // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-    setGreetMsg(await invoke("greet", { name }));
-  }
+	async function greet() {
+		// Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
+		setGreetMsg(await invoke("greet", { name }));
+	}
 
-  return (
-    <div className="container">
-      <h1>Welcome to Tauri!</h1>
+	return (
+		<div className="container">
+			<h1>Welcome to Tauri!</h1>
 
-      <form
-        className="row"
-        onSubmit={(e) => {
-          e.preventDefault();
-          greet();
-        }}
-      >
-        <input
-          id="greet-input"
-          onChange={(e) => setName(e.currentTarget.value)}
-          placeholder="Enter a name..."
-        />
-        <Button type="submit">Greet</Button>
-      </form>
+			<form
+				className="row"
+				onSubmit={(e) => {
+					e.preventDefault();
+					greet();
+				}}
+			>
+				<input
+					id="greet-input"
+					onChange={(e) => setName(e.currentTarget.value)}
+					placeholder="Enter a name..."
+				/>
+				<Button type="submit">Greet</Button>
+			</form>
 
-      <p>{greetMsg}</p>
-      <Card className="mx-auto max-w-sm">
-        <CardHeader>
-          <CardTitle className="text-2xl">Connect Your eMail</CardTitle>
-          <CardDescription>
-            click the link below
-          </CardDescription>
-        </CardHeader>
-        <CardContent>
-          <div className="grid gap-4">
-            <div className="grid gap-2">
-              <div className="flex items-center space-x-1">
-                <Avatar>
-                  <AvatarImage src="https://github.com/shadcn.png" />
-                  <AvatarFallback>Google</AvatarFallback>
-                </Avatar>
-                <Button className="w-4/5">
-                  <a href="https://www.baidu.com">Google</a>
-                </Button>
-              </div>
+			<p>{greetMsg}</p>
+			<Card className="mx-auto max-w-sm">
+				<CardHeader>
+					<CardTitle className="text-2xl">Connect Your eMail</CardTitle>
+					<CardDescription>click the link below</CardDescription>
+				</CardHeader>
+				<CardContent>
+					<div className="grid gap-4">
+						<div className="grid gap-2">
+							<div className="flex items-center space-x-1">
+								<Avatar>
+									<AvatarImage src="https://github.com/shadcn.png" />
+									<AvatarFallback>Google</AvatarFallback>
+								</Avatar>
+								<Button className="w-4/5">
+									<a href="https://www.baidu.com">Google</a>
+								</Button>
+							</div>
 
-              <div className="flex items-center space-x-1">
-                <Avatar>
-                  <AvatarImage src="https://github.com/shadcn.png" />
-                  <AvatarFallback>Outlook</AvatarFallback>
-                </Avatar>
-                <Button className="w-4/5">
-                  <a href="https://www.baidu.com">Outlook</a>
-                </Button>
-              </div>
+							<div className="flex items-center space-x-1">
+								<Avatar>
+									<AvatarImage src="https://github.com/shadcn.png" />
+									<AvatarFallback>Outlook</AvatarFallback>
+								</Avatar>
+								<Button className="w-4/5">
+									<a href="https://www.baidu.com">Outlook</a>
+								</Button>
+							</div>
 
-              <OpenLink></OpenLink>
-
-            </div>
-          </div>
-
-        </CardContent>
-      </Card>
-
-    </div>
-  );
+							<OpenLink />
+						</div>
+					</div>
+				</CardContent>
+			</Card>
+		</div>
+	);
 }
 
 export default App;
--- src/components/openlink.tsx
+++ src/components/openlink.tsx
@@ -1,21 +1,27 @@
-import React from "react";
-import { open } from "@tauri-apps/plugin-shell";
 import { invoke } from "@tauri-apps/api/core";
+import { open } from "@tauri-apps/plugin-shell";
+import type React from "react";
 
 const OpenLink: React.FC = () => {
-  // 为 url 参数指定类型
-  const openInBrowser = async (url: string): Promise<void> => {
-    await invoke("new_server", {})
-    await open(url);
-  };
+	// 为 url 参数指定类型
+	const openInBrowser = async (url: string): Promise<void> => {
+		await invoke("new_server", {});
+		await open(url);
+	};
 
-  return (
-    <div>
-      <button onClick={() => openInBrowser("https://accounts.google.com/o/oauth2/auth?client_id=632451831672-mfg1ol2lofb8ntf9og1eblkmgc81hv70.apps.googleusercontent.com&redirect_uri=http://localhost:8080/callback&scope=https://mail.google.com&response_type=code")}>
-        Open Outlook in Browser
-      </button>
-    </div>
-  );
+	return (
+		<div>
+			<button
+				onClick={() =>
+					openInBrowser(
+						"https://accounts.google.com/o/oauth2/auth?client_id=632451831672-mfg1ol2lofb8ntf9og1eblkmgc81hv70.apps.googleusercontent.com&redirect_uri=http://localhost:8080/callback&scope=https://mail.google.com&response_type=code",
+					)
+				}
+			>
+				Open Outlook in Browser
+			</button>
+		</div>
+	);
 };
 
 export default OpenLink;
